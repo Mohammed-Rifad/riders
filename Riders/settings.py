7bfe0892e082b012d4a6be61d5902fc4549ef5e8
Mohammed-Rifad <mohammedrifad17@gmail.com>
2022-04-04 11:59:34 +0530

    login added

--- Riders/settings.py
+++ Riders/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-apsy21*(q&(loj*++dpp+g##m!=wny$k9ru-6m-is5zk1!=d8b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
 ]
 
 MIDDLEWARE = [
