85ca2f48088d4a824f40f2dedde74cabe2b4f9e5
Mohammed-Rifad <mohammedrifad17@gmail.com>
2022-04-08 09:32:13 +0530

    view sales with dummy data

--- riders_admin/urls.py
+++ riders_admin/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.login),
     path('dashboard', views.dashboard,name='dashboard'),
-    path('add-expense',views.add_expense)
+    path('add-expense',views.add_expense),
+    path('view-expense',views.view_expense)
 ]
--- riders_admin/views.py
+++ riders_admin/views.py
@@ -6,30 +6,45 @@ from datetime import datetime
 
 
 def login(request):
-    authenticated=True
-    if request.method=='POST':
-        name=request.POST['user_name']
-        passwd=request.POST['user_passwd']
-        is_authenticated=Admin.objects.filter(user_name=name,user_passwd=passwd).exists()
+    authenticated = True
+    if request.method == 'POST':
+        name = request.POST['user_name']
+        passwd = request.POST['user_passwd']
+        is_authenticated = Admin.objects.filter(
+            user_name=name, user_passwd=passwd).exists()
         if is_authenticated:
             return redirect('dashboard')
         else:
             print('Not')
-            authenticated=False
-    return render(request,'login.html',{'authenticated':authenticated})
+            authenticated = False
+    return render(request, 'login.html', {'authenticated': authenticated})
+
 
 def dashboard(request):
-    return render(request,'add_expense.html')
+    return render(request, 'dashboard.html')
+
 
 def add_expense(request):
-    name=request.POST['n']
-    dt=request.POST['dt']
-    amount=request.POST['amt']
+    if request.method=='POST':
+        name = request.POST['n']
+        dt = request.POST['dt']
+        amount = request.POST['amt']
 
-    print(name,amount,dt)
-    obj=datetime.strptime(dt,'%Y-%m-%d')
-    new_date=str(obj.day)+'/'+str(obj.month)+'/'+str(obj.year)
-    data=Expense(name=name,dt=new_date,amount=amount)
-    data.save()
-    status='Expense Added'
-    return JsonResponse({'status':status})
+        print(name, amount, dt)
+        obj = datetime.strptime(dt, '%Y-%m-%d')
+        new_date = str(obj.day)+'/'+str(obj.month)+'/'+str(obj.year)
+        data = Expense(name=name, dt=new_date, amount=amount)
+        data.save()
+        status = 'Expense Added'
+        return JsonResponse({'status': status})
+    return render(request,'add_expense.html')
+
+def view_expense(request):
+    data_set = 0
+    print('***************************')
+    if request.method == 'POST':
+        dt = request.POST['date']
+        obj = datetime.strptime(dt, '%Y-%m-%d')
+        search_date = str(obj.day)+'/'+str(obj.month)+'/'+str(obj.year)
+        data_set = Expense.objects.filter(dt=search_date)
+    return render(request, 'view_expenses.html', {'expenses': data_set})
